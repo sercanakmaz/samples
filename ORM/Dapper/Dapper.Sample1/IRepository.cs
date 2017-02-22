70a4fbe2388c257f76ac5d967b755753ae1e87c0
srjohn <sercan.akmaz@gmail.com>
2017-02-22 18:05:43 +0200

    dapper sample1 created

---
+++ ORM/Dapper/Dapper.Sample1/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dapper.Sample1
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        TEntity Get(dynamic id);
+        IEnumerable<TEntity> GetAll();
+        int Insert(TEntity obj);
+        int Insert(IEnumerable<TEntity> list);
+        bool Update(TEntity obj);
+        bool Update(IEnumerable<TEntity> list);
+        bool Delete(TEntity obj);
+        bool Delete(IEnumerable<TEntity> list);
+        bool DeleteAll();
+    }
+}
---
+++ ORM/Dapper/Dapper.Sample1/Repository.cs
@@ -0,0 +1,80 @@
+﻿using Dapper.Contrib;
+using Dapper.Contrib.Extensions;
+using JohnsonNet;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dapper.Sample1
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected IDbConnection Connection
+        {
+            get
+            {
+                var connection = JohnsonManager.Config.Current.GetConnection("LocalSqlServer");
+                connection.Open();
+
+                return connection;
+            }
+        }
+
+        public bool Delete(IEnumerable<TEntity> list)
+        {
+            List<bool> results = new List<bool>();
+
+            foreach (var item in list)
+            {
+                Connection.Delete(item);
+            }
+
+            return results.All(p => p);
+        }
+
+        public bool Delete(TEntity obj)
+        {
+            return Connection.Delete(obj);
+        }
+
+        public bool DeleteAll()
+        {
+            return Connection.DeleteAll<TEntity>();
+        }
+
+        public TEntity Get(dynamic id)
+        {
+            var result = SqlMapperExtensions.Get<TEntity>(Connection, id);
+
+            return result;
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(IEnumerable<TEntity> list)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(TEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(IEnumerable<TEntity> list)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(TEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
