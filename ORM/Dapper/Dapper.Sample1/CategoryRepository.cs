f9d10a27b274f87a0cff33bb63c16798c3cd90b0
srjohn <sercan.akmaz@gmail.com>
2017-02-27 14:42:28 +0200

    #dapper / categoryrepository added

---
+++ ORM/Dapper/Dapper.Sample1/CategoryRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DapperExtensions;
+
+namespace Dapper.Sample1
+{
+    public class CategoryRepository : Repository<Categories, int>
+    {
+        protected override IFieldPredicate KeyPredicate(int key)
+        {
+            return Predicates.Field<Categories>(p => p.CategoryID, Operator.Eq, key);
+        }
+
+        protected override IFieldPredicate KeyPredicate(Categories entity)
+        {
+            return Predicates.Field<Categories>(p => p.CategoryID, Operator.Eq, entity.CategoryID);
+        }
+    }
+}
