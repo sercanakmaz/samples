3f82df97317cebd7fefe07587b6555f55b6ce44d
srjohn <sercan.akmaz@gmail.com>
2017-04-23 23:57:54 +0200

    listview sample

--- ReactNative/HelloWorld/index.android.js
+++ ReactNative/HelloWorld/index.android.js
@@ -5,27 +5,48 @@
  */
 
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry, StyleSheet, ListView, Text, View, Image } from 'react-native';
 
 export default class HelloWorld extends Component {
+    constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows([
+        {
+          title: "Sarı küçük kedicik",
+          type: "Kedi",
+          location: "Istanbul, Turkey",
+          imageSource: "http://www.bempar.com/Uploads/Product/Images/25kuyl1p-340x260.jpg",
+          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...."
+        },
+        {
+          title: "Yavru kangal köpecik",
+          type: "Köpek",
+          location: "Istanbul, Turkey",
+          imageSource: "http://www.bempar.com/Uploads/Product/Images/o0wroql1-340x260.jpg",
+          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...."
+        }
+      ])
+    };
+  }
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+   return (
+      <View style={{flex: 1, paddingTop: 22}}>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => 
+            <View style={{flexDirection: "row"}}>
+              <Text style={{flex:2}}>{rowData.title}</Text>
+              <Image
+                  resizeMode="contain"
+                  style={{width: 50, height: 50}}
+                  source={{ uri: rowData.imageSource}}
+                />
+              <Text style={{flex:1}}>{rowData.type}</Text>
+              <Text style={{flex:1}}>{rowData.location}</Text>
+            </View>}
+        />
       </View>
     );
   }
