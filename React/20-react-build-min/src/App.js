c71fbb30b0aea333d1049b5f9fbb2dae2fbb6447
Sercan <jim@ffsnxg.com>
2018-04-17 19:11:29 +0300

    -

--- React/20-react-build-min/src/App.js
+++ React/20-react-build-min/src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import tinymce from 'tinymce';
+
+import 'tinymce/themes/modern';
+import 'tinymce/plugins/wordcount';
+import 'tinymce/plugins/table';
+import 'tinymce/plugins/code';
 
 class App extends Component {
   render() {
