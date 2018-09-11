21afdf98c7943060ee171c03797e3a1374025b91
Sercan <jim@ffsnxg.com>
2018-09-11 19:23:34 +0300

    hello blockchain example

---
+++ BlockChain/BlockChain.Host/TransactionHub.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlockChain.Host
+{
+    public class TransactionHub : Microsoft.AspNetCore.SignalR.Hub
+    {
+        public async Task SendMessage(string message)
+        {
+            await Clients.All.SendCoreAsync("ReceiveMessage", new object[] { message });
+        }
+    }
+}
---
+++ BlockChain/BlockChain.TestConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading;
+
+namespace BlockChain.TestConsoleApp1
+{
+    class Program
+    {
+        static HubConnection connection;
+        static void Main(string[] args)
+        {
+            connection = new HubConnectionBuilder()
+                       .WithUrl("http://localhost:54816/transactions")
+                       .Build();
+
+            connection.StartAsync();
+
+            new Thread(() =>
+            {
+                connection.On<string>("ReceiveMessage", (message) =>
+                {
+                    Console.WriteLine(message);
+                });
+            }).Start();
+
+            new Thread(() =>
+            {
+                for (int i = 0; i < 500; i++)
+                {
+                    connection.SendAsync("SendMessage", $"test{i}");
+                    Thread.Sleep(500);
+                }
+            }).Start();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
